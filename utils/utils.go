aebfaf506e9eb8e57e467d19b00387279cf203ea
E. Dunham <github@edunham.net>
2025-04-22 16:50:45 -0700

    file handling codegen changes

--- utils/utils.go
+++ utils/utils.go
@@ -15,10 +15,30 @@ import (
 	"github.com/pb33f/libopenapi/orderedmap"
 )
 
+func ExtractIDs(jsonData []byte) ([]string, error) {
+	var items []map[string]interface{}
+	if err := json.Unmarshal(jsonData, &items); err != nil {
+		return nil, fmt.Errorf("failed to parse JSON: %w", err)
+	}
+
+	var ids []string
+	for _, item := range items {
+		if id, ok := item["id"].(string); ok {
+			ids = append(ids, id)
+		}
+	}
+
+	return ids, nil
+}
+
 func WriteFile(file *os.File, filePath, tmpFile string, data map[string]interface{}) error {
 	defer file.Close()
 	w := bufio.NewWriter(file)
-	funcs := template.FuncMap{"join": strings.Join}
+	funcs := template.FuncMap{
+		"join":      strings.Join,
+		"lower":     strings.ToLower,
+		"hasPrefix": strings.HasPrefix,
+	}
 	cmdTmplFilePath := fmt.Sprintf("%v/%v", filePath, tmpFile)
 	tmpl, err := template.New(tmpFile).Funcs(funcs).ParseFiles(cmdTmplFilePath)
 	if err != nil {
@@ -110,4 +130,43 @@ func ExtractMap(payload string) (map[string]interface{}, error) {
 	m := make(map[string]interface{})
 	err := json.Unmarshal([]byte(payload), &m)
 	return m, err
+}
+
+func PrepareDataForRestore(jsonData []byte) ([]byte, error) {
+	var data map[string]interface{}
+	if err := json.Unmarshal(jsonData, &data); err != nil {
+		return nil, fmt.Errorf("failed to parse backup data: %w", err)
+	}
+
+	removeMetadataFields(data)
+
+	cleanedData, err := json.Marshal(data)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal processed data: %w", err)
+	}
+
+	return cleanedData, nil
+}
+
+func removeMetadataFields(data map[string]interface{}) {
+	fieldsToRemove := []string{
+		"_links", "_embedded", "id", "created", "lastUpdated",
+		"lastLogin", "passwordChanged", "status",
+	}
+
+	for _, field := range fieldsToRemove {
+		delete(data, field)
+	}
+
+	for _, value := range data {
+		if nestedMap, ok := value.(map[string]interface{}); ok {
+			removeMetadataFields(nestedMap)
+		} else if nestedSlice, ok := value.([]interface{}); ok {
+			for _, item := range nestedSlice {
+				if nestedMap, ok := item.(map[string]interface{}); ok {
+					removeMetadataFields(nestedMap)
+				}
+			}
+		}
+	}
 }
